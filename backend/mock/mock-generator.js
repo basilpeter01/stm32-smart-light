c9644807ccabf1b0b2289a6bda0caa39d2241d44
Basil <basilpeter2006@gmail.com>
2026-09-22 01:30:55 +0530

    fix: update path for test file

--- backend/mock/mock-generator.js
+++ backend/mock/mock-generator.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const requiredEnvVars = ['MQTT_URL', 'MQTT_USER', 'MQTT_PASSWORD'];
 const missingVars = requiredEnvVars.filter(envVar => !process.env[envVar]);
